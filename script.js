106a16c2a97ef4b3cd2252fe5769dcebbda162fd
Fbyns <florian.bynens@outlook.fr>
2024-07-12 01:48:42 +0200

    features : closeButton, command -clear, new icone to open CLI

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // cibler le body pour qu'au click il y est une redirection sur l'input
-const COMMAND_BODY = document.getElementsByTagName("command_line_body");
-
+const COMMAND_BODY = document.getElementsByClassName("command_line_body");
 // cibler la div pour y générer de nouveaux éléments html
 const NEW_COMMAND_LINE = document.getElementById("command_line");
+// cibler tout l'invit de command
+const ENTIRE_COMMAND = document.getElementById("entire_command");
+// référence au dernier input.value
 let currentAnswerNumber = 0;
+//fermeture au click de l'icone
+const CLOSE_BUTTON = document.getElementById("button_fake_close");
+// init du tableau des commandes
+const COMMAND_TAB = 0;
+// Init du tableau de mes experiences
+const COMMAND_EXP = 0;
+
+document.addEventListener("mousemove", function (event) {
+  console.log(event);
+});
 
 function newElementsResponse() {
   const NEW_Div1 = document.createElement("div");
@@ -15,6 +27,7 @@ function newElementsResponse() {
 
   NEW_Input.setAttribute("type", "text");
   NEW_Input.setAttribute("name", "userinput");
+  NEW_Input.setAttribute("autocomplete", "off");
   NEW_Input.id = "input";
   NEW_Div1.classList.add("command_first_line");
   NEW_Div2.classList.add("command_response");
@@ -24,6 +37,7 @@ function newElementsResponse() {
   NEW_COMMAND_LINE.appendChild(NEW_Div1);
   NEW_COMMAND_LINE.appendChild(NEW_Div2);
   let reference_to_input_user = document.getElementById("input");
+  // Lancer un focus direct sur l'input à la création
   reference_to_input_user.focus();
   reference_to_input_user.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
@@ -31,7 +45,6 @@ function newElementsResponse() {
 
       // INJECTER DU TEXT DANS COMMAND RESPONSE
       let allCommandResponses = document.querySelectorAll(".command_response");
-
       switch (inputUserValue) {
         case "h":
         case "-h":
@@ -39,6 +52,10 @@ function newElementsResponse() {
         case "--help":
           allCommandResponses[currentAnswerNumber].innerText =
             "Ceci est l'aide de mon invite, pour accéder à toutes les commandes : ";
+          break;
+        case "-clear":
+          // Actualise le DOM pour clear le command_body
+          window.location.reload();
           break;
         default:
           allCommandResponses[currentAnswerNumber].innerText =
@@ -52,6 +69,7 @@ function newElementsResponse() {
       newElementsResponse();
     }
   });
+
   document.body.addEventListener("click", function (event) {
     if (event.target.targetName != "INPUT") {
       reference_to_input_user.focus();
@@ -59,9 +77,16 @@ function newElementsResponse() {
   });
 }
 
+// suppression de l'invit de command au click du button
+CLOSE_BUTTON.addEventListener("click", function (event) {
+  event.preventDefault();
+  ENTIRE_COMMAND.remove();
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   // if "enter" is press it does this :
   newElementsResponse();
+
   // recuperer l'input de l'utilisateur dans une variable
 
   // reference_to_input_user.addEventListener("keydown", (event) => {
